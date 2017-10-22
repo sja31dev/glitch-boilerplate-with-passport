0ccbf74dc80dab4c5d7825e945767ef4c94788c1
Glitch (plump-building) <none>
2017-10-22 21:39:57 +0000

    Integrated Facebook, twitter and github

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ var passport = require('passport');
 var session = require('express-session');
 var mongoose = require('mongoose');
 
+var passportGithub = require('./auth/github');
+var passportTwitter = require('./auth/twitter');
+var passportFacebook= require('./auth/facebook');
+
 var app = express();
-
-var Schema = mongoose.Schema;
-
 
 // express-session and passport middleware
 app.use(session({
@@ -23,13 +24,40 @@ app.use(passport.session());
 // *** mongoose *** //
 mongoose.connect('mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.MONGODB_SERVER + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DB, {useMongoClient: true});
 
-// create User Schema
-var User = new Schema({
-  name: String,
-  someID: String
-});
+// ROUTES
 
-module.exports = mongoose.model('users', User);
+//GITHUB
+app.get('/auth/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }));
+app.get('/auth/github/callback',
+  passportGithub.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication
+    res.json(req.user);
+  });
+
+//TWITTER
+app.get('/auth/twitter', passportTwitter.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+  passportTwitter.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication
+    res.json(req.user);
+  });
+
+//FACEBOOK
+app.get('/auth/facebook', passportFacebook.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passportFacebook.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication
+    res.json(req.user);
+  });
+
+app.get('/api/me',
+  passport.authenticate('digest', { session: false }),
+  function(req, res) {
+    res.json(req.user);
+  });
 
 app.get('/login', function(req, res, next) {
   res.send('Go back and register!');
@@ -41,10 +69,9 @@ app.get('/:string', function(req, res) {
 });
 
   
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 
 // Respond not found to all the wrong routes
